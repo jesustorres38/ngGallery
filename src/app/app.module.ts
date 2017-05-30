6bb2195aa48509895cb86d2961e075cdc7bff98e
Jesus Torres <jesustorres38@hotmail.com>
2017-05-30 05:04:52 +0200

    login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,35 @@ import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
+import { ZwitterComponent } from './zwitter/zwitter.component';
 
-
+const appRoutes: Routes = [
+    {path:'home', component: HomeComponent},
+    {path:'login', component: LoginComponent},
+    {path:'', redirectTo:'home', pathMatch:'full' },
+    {path:'**',component: NotFoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    NotFoundComponent,
+    LoginComponent,
+    ZwitterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/zwitter/zwitter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zwitter',
+  templateUrl: './zwitter.component.html',
+  styleUrls: ['./zwitter.component.css']
+})
+export class ZwitterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
